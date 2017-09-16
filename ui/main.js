69ec97812a6b9de3dc1d1a99df83256de60adf6a
muhammathmaushook <30982683+muhammathmaushook@users.noreply.github.com>
2017-09-16 20:59:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 alert('This is second script');
 var element = document.getElementById('free');
 element.innerHTML='Nothing';
-var img =  document.getElementById('imge');
+var img =  document.getElementById('madi');
 img.onclick= function () {
   img.style.marginleft = '200px';
 };
