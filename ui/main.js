0c718f37bf373348598e78ef40ea526d6c4e57e9
muhammathmaushook <30982683+muhammathmaushook@users.noreply.github.com>
2017-09-16 20:51:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ alert('This is second script');
 var element = document.getElementById('free');
 element.innerHTML='Nothing';
 var img =  document.getElementById('img');
-img.onclick= function() {
+img.onclick= function () {
   img.style.marginleft= '100px';
 };
