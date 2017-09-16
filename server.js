8f9720b54aee88eb88a9f0458b84b96370fd2428
muhammathmaushook <30982683+muhammathmaushook@users.noreply.github.com>
2017-09-16 19:01:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/Article one', function (req, res) {
+  res.send('Here is the Article one');
+});
+app.get('/Article two', function (req, res) {
+  res.send('Here is the Article two');
+});
+app.get('/Article three', function (req, res) {
+  res.send('Here is the Article three');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
