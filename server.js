d58a47a3c995afc0b0c8f2d6a3306db6527c0ef6
muhammathmaushook <30982683+muhammathmaushook@users.noreply.github.com>
2017-09-23 19:45:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
   res.sendFile=(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send(templete(articleOne));
+  res.send(templete(articl-eOne));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
